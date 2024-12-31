b9d8dfd136bdfb6a2cf57179e532c1715e2d98da
Andrei Ticarat <75678080+Kerolly@users.noreply.github.com>
2024-12-31 15:25:59 +0200

    Responsive bug fixed & start restaurant page

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,3 @@
-const header = document.querySelector("header");
-const headerTexts = document.querySelectorAll(".logo h2, .logo p");
-const hamMenuColor = document.querySelectorAll(".ham-menu span");
-const hr = document.querySelector("header hr");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-    hr.classList.add("scrolled-hr");
-
-    headerTexts.forEach((element) => {
-      element.classList.add("scrolled-text");
-    });
-
-    hamMenuColor.forEach((element) => {
-      element.classList.add("scrolled-ham");
-    });
-  } else {
-    header.classList.remove("scrolled");
-    hr.classList.remove("scrolled-hr");
-
-    headerTexts.forEach((element) => {
-      element.classList.remove("scrolled-text");
-    });
-
-    hamMenuColor.forEach((element) => {
-      element.classList.remove("scrolled-ham");
-    });
-  }
-});
 
 // --Carousel Scroll--
 
